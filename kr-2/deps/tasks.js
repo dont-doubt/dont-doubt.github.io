6ab07af9d133e5e67f7b47eccd442a00ee9f9fb9
dont-doubt <74961444+dont-doubt@users.noreply.github.com>
2024-05-11 01:14:49 +0800

    Cleanup

--- kr-2/deps/tasks.js
+++ kr-2/deps/tasks.js
@@ -2,11 +2,11 @@
 const tasks = [
   { 
     name: "1",
-    description: 'Вариант: <span class="text-gray-600 font-light">21 % 19 = 2</span><br>Условие: <span class="text-gray-600 font-light">Определить сумму первых десяти членов ряда Фибоначчи</span>'
+    description: 'Вариант: <span class="text-gray-600 font-light">21 % 19 = 2</span>'
   },
   {
     name: "2",
-    description: 'Вариант: <span class="text-gray-600 font-light">21 % 14 = 7</span><br>Условие: <span class="text-gray-600 font-light">Сформировать матрицу М1, вычтя из каждого элемента исходной матрицы М среднее арифметическое, вычисленное по всем ее элементам</span>'
+    description: 'Вариант: <span class="text-gray-600 font-light">21 % 14 = 7</span>'
   },
   {
     name: "3",
